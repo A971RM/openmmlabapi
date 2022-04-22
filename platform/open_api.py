ce282dad8a542b030931186e8210ae86903b2df4
Zhao Yuanxing <clive.zhao@aliyun.com>
2022-04-23 06:25:58 +0800

    UPDATE API

--- platform/open_api.py
+++ platform/open_api.py
@@ -3,10 +3,12 @@
 https://platform.openmmlab.com/docs/zh-CN/open-api/
 """
 import hashlib
+import os
 import time
 from collections import OrderedDict
 from urllib.parse import urljoin
 
+import magic
 import requests
 import urllib3.util
 
@@ -34,15 +36,63 @@ def get_access_token():
     Headers = dict(ts=ts, nonce=nonce, sign=sign, accessKey=accessKey)
 
     url = urljoin(Host, Path)
-    ret = requests.post(url, headers=Headers)
-    if ret.ok and ret.json().get('msg', '') == 'ok':
-        accessToken = ret.json()['data']['accessToken'][len('Bearer'):].strip()
+    response = requests.post(url, headers=Headers)
+    if response.ok and response.json().get('msg', '') == 'ok':
+        accessToken = response.json()['data']['accessToken'][len('Bearer'):].strip()
         return accessToken
-    raise ValueError(f"Cannot get {ret.url}")
+    raise ValueError(f"Cannot get {response.url}")
 
 
-def
+def upload_file(accessToken, tag, files):
+    """
+    第三步：上传文件
+    参考：https://platform.openmmlab.com/docs/zh-CN/open-api/steps/upload-file
+    :return:
+    """
+    Host = r"https://platform.openmmlab.com"
+    Path = r"/gw/upload-service/api/v1/uploadFile"
+    Headers = dict(Authorization=accessToken)
+
+    url = urljoin(Host, Path)
+
+    key = "inference"
+    params = dict(key=key, tag=tag)
+    files_ = []
+    for file in files:
+        filename = os.path.basename(file)
+        with open(file, 'rb') as f:
+            body = f.read()
+        content_type = magic.from_file(file, mime=True)
+        files_.append(['file', (filename, body, content_type)])
+
+    response = requests.post(url, params=params, headers=Headers, files=files_)
+    if response.ok and response.json().get("msg", "") == "ok":
+        return response
+    return response
+
+def classification_example(accessToken):
+    """
+    参考： https://platform.openmmlab.com/docs/zh-CN/open-api/guides/image-classification
+    :return:
+    """
+    Host = "https://platform.openmmlab.com"
+    Path = "/gw/model-inference/openapi/v1/classification"
+
+    url = urljoin(Host, Path)
+    Headers = dict(Authorization=accessToken)
+    Body = dict(algorithm="Swin-Transformer",
+                dataset="ImageNet",
+                backend="PyTorch",
+                resource="https://oss.openmmlab.com/web-demo/static/one.b7608e9b.jpg",
+                resourceType="URL",
+                requestType="SYNC"
+                )
+    response = requests.post(url, headers=Headers, json=Body)
+    return response
 
 if __name__ == '__main__':
-    ret = get_access_token()
+    accessToken = get_access_token()
+    ret = upload_file(accessToken=accessToken, tag="segmentation", files=[r'./data/fire.jpg'])
+    print(ret)
+    ret = classification_example(accessToken=accessToken)
     print(ret)
